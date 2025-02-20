a11acf7ecde86407b1f8a2c15c4e671f9278e907
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2025-02-20 17:28:29 +0300

    logout

--- app/main.go
+++ app/main.go
@@ -9,37 +9,46 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-type appContext struct {
-	ctx *context.Context
-}
-
 func main() {
 	mux := http.NewServeMux()
 
-	var ctx *appContext
+	// create context
+	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
+	defer cancel()
 
-	mux.HandleFunc("/login", LoginHandler(ctx))
+	mux.HandleFunc("/login", LoginHandler(&ctx))
+	mux.HandleFunc("/logout", LogoutHandler(&ctx))
 	http.ListenAndServe(":8090", mux)
 }
 
-func LoginHandler(h *appContext) func(http.ResponseWriter, *http.Request) {
+func LoginHandler(ctx *context.Context) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
+		resp := make(map[string]string)
 		// Проверяем метод запроса - для логина принимаем только POST
 		if req.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			resp["status"] = "Error"
+			resp["message"] = "Method not allowed"
+			w.WriteHeader(http.StatusCreated)
+			w.Header().Set("Content-Type", "application/json")
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
 			return
 		}
+
+		log.Println("ctx login1:", *ctx)
 		login := req.PostFormValue("login")
 		password := req.PostFormValue("password")
 
-		// create context
-		ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
-		defer cancel()
+		// Определяем user agent
 		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")
 		// run task list
 		var res string
 		ok := true
-		err := chromedp.Run(ctx,
+		// Проходим авторизацию в почту и возвращаем текст с количеством писем
+		err := chromedp.Run(*ctx,
 			chromedp.Navigate(`https://mail.rambler.ru/`),
 			chromedp.WaitVisible(`#login`),
 			chromedp.SendKeys(`#login`, login),
@@ -53,21 +62,69 @@ func LoginHandler(h *appContext) func(http.ResponseWriter, *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		// формируем статус ответа
 		w.WriteHeader(http.StatusCreated)
 		w.Header().Set("Content-Type", "application/json")
-		resp := make(map[string]string)
 
 		if len(res) > 0 {
 			resp["status"] = "Ok"
 			resp["message"] = res
 		} else {
 			resp["status"] = "Error"
-			resp["message"] = res
+			resp["message"] = "Response is empty"
 		}
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		}
 		w.Write(jsonResp)
+	}
+}
+
+func LogoutHandler(ctx *context.Context) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		resp := make(map[string]string)
+
+		if ctx == nil {
+			resp["status"] = "Error"
+			resp["message"] = "Browser context is empty"
+			w.WriteHeader(http.StatusCreated)
+			w.Header().Set("Content-Type", "application/json")
+			jsonResp, err := json.Marshal(resp)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+			}
+			w.Write(jsonResp)
+			return
+		}
+		var res string
+		err := chromedp.Run(*ctx,
+			chromedp.WaitVisible(`//div[@class="rc__mAFe4"]/div[@class="rc__xgBcB"]/button`, chromedp.BySearch),
+			chromedp.Click(`//div[@class="rc__mAFe4"]/div[@class="rc__xgBcB"]/button`, chromedp.BySearch),
+			chromedp.WaitVisible(`//div[@class="rc__IP0ui"]/div[4]/button`, chromedp.BySearch),
+			chromedp.Click(`//div[@class="rc__IP0ui"]/div[4]/button`, chromedp.BySearch),
+			chromedp.WaitVisible(`.ad_branding_site`, chromedp.ByQuery),
+			chromedp.Location(&res),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+
+		if len(res) > 0 {
+			resp["status"] = "Ok"
+			resp["message"] = res
+		} else {
+			resp["status"] = "Error"
+			resp["message"] = "Response is empty"
+		}
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		}
+		w.Write(jsonResp)
+
 	}
 }
